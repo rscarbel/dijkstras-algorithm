5a28db0d49238d10a7b3abc686a9d3c42dbbe2be
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-09 15:58:04 -0500

    removed unnecessary comment clutter

--- src/dataStructures/graph.js
+++ src/dataStructures/graph.js
@@ -1,23 +1,3 @@
-  /*
-  new node
-
-  graph.nodes {
-    A: [ {node: A, weight: 0}, {node: B, weight, 1} ]
-    B: [ {node: B, weight: 0}, {node: A, weight, 1}, {node: C, weight: 1} ]
-    C: [ {node: C, weight: 0}, {node: B, weight, 1} ]
-  }
-
-    graph.nodes {
-      A: {A: 0, B: 1}
-      B: {B: 0, A: 1, C: 1}
-      C: {C: 0, B: 1}
-    }
-
-  A: {A: 0, B: 1}
-
-  graph.addNode(name, [])
-  */
-
 class Graph {
   constructor (node){
     this.nodes = {};
