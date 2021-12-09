9f2889e163803ef3497d227f3164326198e00d7c
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-09 16:44:12 -0500

    Added loop to put all nodes on the dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import DikstrasAlgorithm from './components/DikstrasAlgorithm'
 import React from 'react';
+import Node from './components/Node';
 import './App.css';
+import sampleGraph from './components/sampleGraph';
 
 function App() {
+  let nodes = Object.keys(sampleGraph.nodes)
   return (
     <div className="App">
       <h1>Dikstra's Algorithm</h1>
-      <DikstrasAlgorithm />
+    {nodes.map(e => {
+      return <Node name={e} />
+    })}
     </div>
   );
 }
