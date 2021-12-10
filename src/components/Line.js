23cfd3224efbff8434ed38632b083ec5cc191385
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-10 13:10:41 -0500

    Line component properly renders

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,6 +1,14 @@
-const Line = () => {
-  return <>
-  </>
+const Line = ({left1, left2, right1, right2}) => {
+  const elementStyle = {
+    zIndex: -1,
+    position: 'absolute',
+    top: left1,
+    left: left2
+  }
+  return <svg style={elementStyle}>
+    <polyline fill="none" stroke="#000000" strokeWidth="1"
+    points={`${0},${0},${right1},${right2}`} />
+    </svg>
 };
 
 export default Line;
