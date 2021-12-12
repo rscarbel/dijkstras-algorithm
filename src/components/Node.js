7b27b34e0c4915f678d991484c8bf2c2d508f6db
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-11 23:44:13 -0500

    set z-index to 1

--- src/components/Node.js
+++ src/components/Node.js
@@ -3,12 +3,13 @@ const Node = ({ name, x, y }) => {
   const divStyle = {
     top: y,
     left: x,
+    zIndex: 1
   };
   return <>
   <div className='node-circle' id={name}  style={divStyle}>
     <svg width='50' height='50'>
       <circle cx='25' cy='25' r='25' fill='gray' />
-      <text x="20" y="29" fill="white">{name}</text>
+      <text x='20' y='29' fill='white'>{name}</text>
     </svg>
   </div>
   </>
