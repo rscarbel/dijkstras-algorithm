bbb6560bead650d2d10ce7fd72d79018e9266e82
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-12 12:39:43 -0500

    centered text better

--- src/components/Node.js
+++ src/components/Node.js
@@ -5,11 +5,12 @@ const Node = ({ name, x, y }) => {
     left: x,
     zIndex: 1
   };
+
   return <>
   <div className='node-circle' id={name}  style={divStyle}>
-    <svg width='50' height='50'>
-      <circle cx='25' cy='25' r='25' fill='gray' />
-      <text x='20' y='29' fill='white'>{name}</text>
+    <svg width='100%' height='100%'>
+      <circle className='node' cx='25' cy='25' r='25' fill='gray' />
+      <text x='20' y='29.5' fill='white'>{name}</text>
     </svg>
   </div>
   </>
