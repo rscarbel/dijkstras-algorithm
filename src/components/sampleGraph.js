5e97259748e53ee35a90c3a28a0b3cf4e186a184
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-11 22:34:07 -0500

    Randomized letters

--- src/components/sampleGraph.js
+++ src/components/sampleGraph.js
@@ -1,38 +1,49 @@
 import Graph from "../dataStructures/graph";
 
-const sampleGraph = new Graph('A');
-sampleGraph.addNode('B');
-sampleGraph.addNode('C');
-sampleGraph.addNode('D');
-sampleGraph.addNode('E');
-sampleGraph.addNode('F');
-sampleGraph.addNode('G');
-sampleGraph.addNode('H');
-sampleGraph.addNode('I');
-sampleGraph.addNode('J');
-sampleGraph.addNode('K');
-sampleGraph.addNode('L');
-sampleGraph.addNode('M');
-sampleGraph.addNode('N');
-sampleGraph.addConnection('A','B',1);
-sampleGraph.addConnection('A','C',1);
-sampleGraph.addConnection('C','D',2);
-sampleGraph.addConnection('C','E',3);
-sampleGraph.addConnection('C','F',2);
-sampleGraph.addConnection('D','E',3);
-sampleGraph.addConnection('E','F',1);
-sampleGraph.addConnection('F','J',2);
-sampleGraph.addConnection('F','G',1);
-sampleGraph.addConnection('E','H',5);
-sampleGraph.addConnection('G','I',1);
-sampleGraph.addConnection('H','L',2);
-sampleGraph.addConnection('H','J',1);
-sampleGraph.addConnection('I','L',1);
-sampleGraph.addConnection('H','G',3);
-sampleGraph.addConnection('K','L',1);
-sampleGraph.addConnection('K','N',1);
-sampleGraph.addConnection('L','N',1);
-sampleGraph.addConnection('L','M',1);
-sampleGraph.addConnection('N','M',2);
+const graphLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N']
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+shuffleArray(graphLetters)
+
+const sampleGraph = new Graph(graphLetters[0]);
+sampleGraph.addNode(graphLetters[1]);
+sampleGraph.addNode(graphLetters[2]);
+sampleGraph.addNode(graphLetters[3]);
+sampleGraph.addNode(graphLetters[4]);
+sampleGraph.addNode(graphLetters[5]);
+sampleGraph.addNode(graphLetters[6]);
+sampleGraph.addNode(graphLetters[7]);
+sampleGraph.addNode(graphLetters[8]);
+sampleGraph.addNode(graphLetters[9]);
+sampleGraph.addNode(graphLetters[10]);
+sampleGraph.addNode(graphLetters[11]);
+sampleGraph.addNode(graphLetters[12]);
+sampleGraph.addNode(graphLetters[13]);
+sampleGraph.addConnection(graphLetters[0],graphLetters[1],1);
+sampleGraph.addConnection(graphLetters[0],graphLetters[2],1);
+sampleGraph.addConnection(graphLetters[2],graphLetters[3],2);
+sampleGraph.addConnection(graphLetters[2],graphLetters[4],3);
+sampleGraph.addConnection(graphLetters[2],graphLetters[5],2);
+sampleGraph.addConnection(graphLetters[3],graphLetters[4],3);
+sampleGraph.addConnection(graphLetters[4],graphLetters[5],1);
+sampleGraph.addConnection(graphLetters[5],graphLetters[9],2);
+sampleGraph.addConnection(graphLetters[5],graphLetters[6],1);
+sampleGraph.addConnection(graphLetters[4],graphLetters[7],5);
+sampleGraph.addConnection(graphLetters[6],graphLetters[8],1);
+sampleGraph.addConnection(graphLetters[7],graphLetters[11],2);
+sampleGraph.addConnection(graphLetters[7],graphLetters[9],1);
+sampleGraph.addConnection(graphLetters[8],graphLetters[11],1);
+sampleGraph.addConnection(graphLetters[7],graphLetters[6],3);
+sampleGraph.addConnection(graphLetters[10],graphLetters[11],1);
+sampleGraph.addConnection(graphLetters[10],graphLetters[13],1);
+sampleGraph.addConnection(graphLetters[11],graphLetters[13],1);
+sampleGraph.addConnection(graphLetters[11],graphLetters[12],1);
+sampleGraph.addConnection(graphLetters[13],graphLetters[12],2);
 
 export default sampleGraph
