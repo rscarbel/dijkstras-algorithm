5f888ad332f29c281234e7d4b4ae62054d3ad6d1
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-13 09:14:59 -0500

    Changed a bunch of things ...
    I spelled vertical right, because like an idiot I had it spelled
    verticle
    
    I added comments to explain why I did everything
    
    I named some variables more descriptively
    
    I lowered the possible spacing of the x-nodes since it could appear off
    the page
    
    I added spacing between lines to make it more readable

--- src/components/GraphDisplay.js
+++ src/components/GraphDisplay.js
@@ -3,46 +3,72 @@ import sampleGraph from './sampleGraph';
 import Line from './Line';
 
 const GraphDisplay = () => {
-  let nodes = Object.keys(sampleGraph.nodes);
-  const generateNumber = (max,min) => Math.floor((Math.random() * max) + min);
-  let height = window.innerHeight - 50;
-  let horizontalSpacing = window.innerWidth / nodes.length
-  let horizontal = 0;
-  let verticle = height/2;
-  let domNodes = [];
-  let domNodesByKey = {};
-  return <>
-    {nodes.forEach(e => {
-            let change = generateNumber((height/3),50)
-            if ((verticle - change) < 50) {
-              verticle += change;
-            }
-            else if ((verticle + change) > (height - 50)){
-              verticle -= change;
-            } else {
-              if (generateNumber(2,0)) {
-                verticle += change;
-              } else {
-                verticle -= change;
-              }
-            }
-            horizontal += generateNumber(horizontalSpacing,26);
-            domNodes.push(<Node key={e} name={e} x={horizontal} y={verticle} />)
-            domNodesByKey[e] = {}
-            domNodesByKey[e].x = horizontal;
-            domNodesByKey[e].y = verticle;
 
-        })}
+  let nodes = Object.keys(sampleGraph.nodes);
+
+  //rng helper function for randomizing graph placement
+  const generateNumber = (max,min) => Math.floor((Math.random() * max) + min);
+
+  //subtract 50 or so from the height to account for the title text
+  let height = window.innerHeight - 50;
+
+  //Allow the largest possible spacing to be limited to screen width
+  let horizontalSpacing = (window.innerWidth / nodes.length) - (50 / nodes.length)
+  //Nodes should start being placed from left to right
+  let horizontal = 0;
+  //the starting vertical alignment will be calculated from a third of the way down the page.
+  let vertical = height/3;
+  //Keep the nodes in an array to display on the dom
+  let domNodes = [];
+  //I'll need to search the values of each node to make connecting lines, so keeping them in an object allows me to find the items using the node name
+  let domNodesByKey = {};
+
+  return <>
+
+    {nodes.forEach(e => {
+      //verticalChange  must be at least 50, because that is the height of the nodes themselves
+      let verticalChange = generateNumber((height/3),50)
+      //keep nodes from being created above the top of the page
+      if ((vertical - verticalChange) < 50) {
+        vertical += verticalChange;
+      }
+      //keep nodes from being created below the bottom of the page
+      else if ((vertical + verticalChange) > (height - 50)){
+        vertical -= verticalChange;
+      } else {
+        if (generateNumber(2,0)) {
+          vertical += verticalChange;
+        } else {
+          vertical -= verticalChange;
+        }
+      }
+
+      horizontal += generateNumber(horizontalSpacing,26);
+
+      domNodes.push(<Node key={e} name={e} x={horizontal} y={vertical} />)
+
+      domNodesByKey[e] = {}
+      domNodesByKey[e].x = horizontal;
+      domNodesByKey[e].y = vertical;
+
+        })};
+
+        {/* Now to add the connecting lines */}
         {domNodes.map(e => {
           const lines = []
           const connectedNodes = Object.keys(sampleGraph.nodes[e.props.name]);
+
+          //One of the keys in each node object is an array keeping track of all nodes with incoming connections, I need to get rid of that since it's not something to connect
           connectedNodes.splice(connectedNodes.indexOf('incomingNodes'),1);
+          //Add 25 to the starting x & y values to place it in the center of the circle
           let startingX = parseInt(e.props.x) + 25;
           let startingY = parseInt(e.props.y) + 25;
+          //Now I need to iterate through each of the connections in the node to see what nodes I need to draw a line to
           connectedNodes.forEach(i => {
             let endingX = parseInt(domNodesByKey[i].x) + 25;
             let endingY = parseInt(domNodesByKey[i].y) + 25;
-            lines.push(<Line left1={startingX} left2={startingY} right1={endingX} right2={endingY} weight={sampleGraph.nodes[e.props.name][i]} width={parseInt(sampleGraph.nodes[e.props.name][i]) ** 1.1} />)
+
+            lines.push(<Line startingX={startingX} startingY={startingY} endingX={endingX} endingY={endingY} weight={sampleGraph.nodes[e.props.name][i]} width={parseInt(sampleGraph.nodes[e.props.name][i]) ** 1.1} />)
           })
           return lines;
         })}
