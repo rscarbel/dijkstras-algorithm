391b7e447fb7295f59a0fd9b12e4c3f273a74dc9
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-13 18:53:56 -0500

    testing if min binary heap is an array

---
+++ src/dataStructures/MinBinaryHeap.test.js
@@ -0,0 +1,9 @@
+import MinBinaryHeap from "./MinBinaryHeap";
+
+describe ('Binary Heap initialization', () => {
+
+  test ('values should be an array', () => {
+    let temp = new MinBinaryHeap();
+    expect(Array.isArray(temp.values)).toBe(true);
+  });
+});
