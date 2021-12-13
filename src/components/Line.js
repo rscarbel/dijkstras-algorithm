7a0b59ad8b16f8e84328d602850ed226aa94d026
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-13 09:16:44 -0500

    Renamed some props to be more descriptive

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,4 +1,4 @@
-const Line = ({left1, left2, right1, right2, width, weight }) => {
+const Line = ({startingX, startingY, endingX, endingY, width, weight }) => {
   let rgbVal = width * 36;
   const svgStyle = {
     position: 'absolute',
@@ -21,16 +21,16 @@ const Line = ({left1, left2, right1, right2, width, weight }) => {
   return<>
     <svg style={svgStyle} width='100%' height='100%'>
       <line style={lineStyle} fill="none" stroke={`rgb(${rgbVal},0,0)`} strokeWidth={width}
-      x1={left1}
-      y1={left2}
-      x2={right1}
-      y2={right2} />
+      x1={startingX}
+      y1={startingY}
+      x2={endingX}
+      y2={endingY} />
 
 
       </svg>
       {width ? <svg style={circleStyle} width='100%' height='100%'>
-      <circle style={circleStyle} cx={(right1 + left1)/2} cy={(right2 + left2)/2} r='0.4em' fill={`rgb(${rgbVal},0,0)`} stroke='#000000'/>
-      <text style={textStyle} x={(right1 + left1)/2 - 2.5} y={(right2 + left2)/2 + 2.5} fill='#FFFFFF'>{weight ? weight : ''}</text>
+      <circle style={circleStyle} cx={(endingX + startingX)/2} cy={(endingY + startingY)/2} r='0.4em' fill={`rgb(${rgbVal},0,0)`} stroke='#000000'/>
+      <text style={textStyle} x={(endingX + startingX)/2 - 2.5} y={(endingY + startingY)/2 + 2.5} fill='#FFFFFF'>{weight ? weight : ''}</text>
       </svg> : ''}
     </>
 };
