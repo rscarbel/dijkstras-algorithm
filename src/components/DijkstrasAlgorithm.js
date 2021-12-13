5a74740b03f049ad0b6220989929e131b98d0334
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-13 10:22:59 -0500

    Spelled dijkstra correctly

--- src/components/DijkstrasAlgorithm.js
+++ src/components/DijkstrasAlgorithm.js
@@ -1,11 +1,11 @@
 import sampleGraph from "./sampleGraph";
 
 
-const DikstrasAlorithm = (props) => {
+const DijkstrasAlorithm = (props) => {
   let exampleGraph = JSON.stringify(sampleGraph,null,2)
   return <p>
     {exampleGraph}
     </p>
 };
 
-export default DikstrasAlorithm;
+export default DijkstrasAlorithm;
