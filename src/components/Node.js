5ab27aa07372bea6af02c127822aa3e93c4c9ba0
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-13 12:07:54 -0500

    Nodes are see-through

--- src/components/Node.js
+++ src/components/Node.js
@@ -3,13 +3,13 @@ const Node = ({ name, x, y }) => {
   const divStyle = {
     top: y,
     left: x,
-    zIndex: 1
+    zIndex: 2
   };
 
   return <>
   <div className='node-circle' id={name}  style={divStyle}>
     <svg>
-      <circle className='node' cx='25' cy='25' r='25' fill='gray' />
+      <circle className='node' cx='25' cy='25' r='25' fill='#282c34' opacity={0.9} />
       <text x='20' y='29.5' fill='white'>{name}</text>
     </svg>
   </div>
