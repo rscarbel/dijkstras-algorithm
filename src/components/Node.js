7732dfbf6105dc4ae67e2c320ad4869fc0a4d482
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-15 12:42:12 -0500

    Made conditionals for the font sizing

--- src/components/Node.js
+++ src/components/Node.js
@@ -9,8 +9,28 @@ const Node = ({ name, x, y }) => {
   return <>
   <div className='node-circle' id={name}  style={divStyle}>
     <svg>
-      <circle className='node' cx='25' cy='25' r='25' fill='#282c34' opacity={0.8} />
-      <text x='20' y='29.5' fill='white'>{name}</text>
+      <circle
+      className='node'
+      cx='25'
+      cy='25'
+      r='25'
+      fill='#282c34'
+      opacity={0.8}
+      />
+
+      <text
+      fontSize={name.length === 1
+        ? '1em'
+        : name.lenth < 6 ? '16px'
+        : name.length < 8 ? '10.5px'
+        : name.length < 11 ? '8px'
+        : name.length < 13 ? '7.5px'
+        : name.length < 15 ? '7px'
+        : '7px' }
+      x={name.length > 1 ? '5': '20'}
+      y='29.5'
+      fill='white'
+      >{name}</text>
     </svg>
   </div>
   </>
