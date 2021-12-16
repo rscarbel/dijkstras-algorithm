ca22a7693fe19a5649744df93ec1b66d9265c42d
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-16 11:27:08 -0500

    binary heap now balances sibling elements

--- src/dataStructures/MinBinaryHeap.js
+++ src/dataStructures/MinBinaryHeap.js
@@ -8,16 +8,45 @@ class MinBinaryHeap {
 
     let index = this.values.length - 1
 
-    while (index > 0) {
+    //balance with sibling {
+    if (!(index % 2) && (index > 0)) {
+      let siblingValue = this.values[index - 1];
+      if (siblingValue > value) {
+        this.values[index] = siblingValue;
+        this.values[index - 1] = value;
+        index --
+      }
+    }
+
+    while (index > 0 ) {
+
+      //balance with sibling
+      if (index % 2) {
+        let siblingValue = this.values[index + 1];
+        if (siblingValue < value) {
+          this.values[index] = siblingValue;
+          this.values[index + 1] = value;
+          index ++
+        }
+      } else {
+        let siblingValue = this.values[index - 1];
+        if (siblingValue > value) {
+          this.values[index] = siblingValue;
+          this.values[index - 1] = value;
+          index --
+        }
+      }
+
       let parentIndex = Math.floor((index - 1)/2);
       let parentValue = this.values[parentIndex];
 
       if (value >= parentValue) {
-        break
-      }
+        break;
+      };
 
       this.values[index] = parentValue;
       this.values[parentIndex] = value;
+
 
       index = parentIndex;
     }
