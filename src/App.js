7a177b19a58c8483c4626ff3ea93813d386e5d7a
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-17 11:55:49 -0500

    console logging the output of djikstra algo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import GraphDisplay from './components/GraphDisplay';
 import './App.css';
 import TopBar from './components/TopBar';
+// import {useState } from 'react';
+import DijkstrasAlorithm from './scripts/DijkstrasAlgorithm';
+import randomGraph from './scripts/randomGraph';
 
 function App() {
-
+  let keys = []
+  for (let key in randomGraph.nodes) {
+    keys.push(key)
+  }
+  let shortestPath = DijkstrasAlorithm(randomGraph,keys[0],keys[0])
   return (
     <div className="App">
         <div className='relative-container'>
           <TopBar />
-          <GraphDisplay />
+          <GraphDisplay displayedGraph={randomGraph} />}
+          {console.log(shortestPath)}
       </div>
     </div>
   );
