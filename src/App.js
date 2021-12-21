fbc68352fc89e8a38548f7d36f16093aab8fa1e1
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-21 17:22:22 -0500

    Added some console logs

--- src/App.js
+++ src/App.js
@@ -6,20 +6,25 @@ import DijkstrasAlorithm from './scripts/DijkstrasAlgorithm';
 import randomGraph from './scripts/randomGraph';
 
 function App() {
-  const [graph, setGraph] = useState(<GraphDisplay displayedGraph={randomGraph} />)
+  const theGraph = randomGraph;
+  const [graph, setGraph] = useState(<GraphDisplay displayedGraph={theGraph} />)
   const reloadGraph = () => {
-    setGraph(<GraphDisplay displayedGraph={randomGraph} />);
+    setGraph(<GraphDisplay displayedGraph={theGraph} />);
   }
   let keys = []
-  for (let key in randomGraph.nodes) {
+  for (let key in theGraph.nodes) {
     keys.push(key)
   }
-  let shortestPath = DijkstrasAlorithm(randomGraph,keys[0],keys[0])
+  let shortestPath = DijkstrasAlorithm(theGraph,keys[0],keys[keys.length - 1]);
+  const [startNode, setStartNode] = useState(keys[0]);
+  const [endNode, setEndNode] = useState(keys[keys.length - 1]);
+  const path = shortestPath[0].print() ? shortestPath[0].print().join(' --> ') : 'No Path Exists';
   return (
     <div className="App">
         <div className='relative-container'>
-          <TopBar reloadAction={reloadGraph} />
+          <TopBar startNode={startNode} endNode={endNode} path={path} reloadAction={reloadGraph} />
           {graph}
+          {console.log(theGraph)}
           {console.log(shortestPath)}
       </div>
     </div>
