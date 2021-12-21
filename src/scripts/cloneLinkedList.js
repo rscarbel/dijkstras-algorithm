0fe34e1e9c1f5b1dc93f8154e98289d28cacf819
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-21 14:35:34 -0500

    made function to make copy of linked lists

---
+++ src/scripts/cloneLinkedList.js
@@ -0,0 +1,15 @@
+import DoublyLinkedList from '../dataStructures/DoublyLinkedList';
+
+const cloneLinkedList = (list) => {
+  let head = list.head;
+  let nextNode = head.next
+  const newList = new DoublyLinkedList();
+  newList.addNode(head.name,0)
+  while (nextNode) {
+    newList.addNode(nextNode.name, nextNode.weight);
+    nextNode = nextNode.next;
+  }
+  return newList
+}
+
+export default cloneLinkedList;
