e50f1f2ecbccea6be115fd5bfa0c5bb18132346d
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-22 12:00:36 -0500

    nodes for pathfinding are now in state

--- src/App.js
+++ src/App.js
@@ -20,21 +20,48 @@ function App() {
   };
 
   let shortestPath = DijkstrasAlorithm(graph,keys[0],keys[keys.length - 1]);
-  // const [startNode, setStartNode] = useState(keys[0]);
-  const startNode = keys[0];
-  // const [endNode, setEndNode] = useState(keys[keys.length - 1]);
-  const endNode = keys[keys.length - 1];
+
+  const [startNode, setStartNode] = useState(keys[0]);
+
+  const selectStartNode = (newNode) => {
+    setStartNode(newNode)
+  }
+
+  const [endNode, setEndNode] = useState(keys[keys.length - 1]);
+
+  const selectEndNode = (newNode) => {
+    setEndNode(newNode)
+  }
+
+  const [selectionMode, setSelectionMode] = useState(false);
+
+  const toggleSelectionMode = () => {
+    setSelectionMode(!selectionMode)
+  }
 
   const path = shortestPath[0] ? shortestPath[0].print().join(' --> ') : 'No Path Exists';
 
   return (
     <div className="App">
-        <div className='relative-container'>
-          <TopBar startNode={startNode} endNode={endNode} path={path} reloadAction={reloadGraph} />
-          <GraphDisplay displayedGraph={graph} />
-          {console.log(graph)}
-          {console.log(shortestPath)}
-      </div>
+      <TopBar
+        startNode={startNode}
+        selectionMode={selectionMode}
+        toggleSelectionMode={toggleSelectionMode}
+        endNode={endNode}
+        path={path}
+        reloadAction={reloadGraph}
+      />
+
+      <GraphDisplay
+        displayedGraph={graph}
+        selectionMode={selectionMode}
+        toggleSelectionMode={toggleSelectionMode}
+        selectStartNode={selectStartNode}
+        selectEndNode={selectEndNode}
+      />
+
+      {console.log(graph)}
+      {console.log(shortestPath)}
     </div>
   );
 }
