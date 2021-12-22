46a6662176f4e49ff9a631d7185e4d6851f0c137
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-22 10:20:18 -0500

    adjusted for refactors

--- src/scripts/DijkstrasAlgorithm.test.js
+++ src/scripts/DijkstrasAlgorithm.test.js
@@ -21,64 +21,64 @@ exampleGraph.addConnection('E', 'F', 2);
 exampleGraph.addConnection('E', 'D', 4);
 exampleGraph.addConnection('G', 'D', 10);
 
-describe ('Dijkstra\'s algorithm should return a doubly linked list', () => {
-  let temp = DijkstrasAlorithm(exampleGraph,'A','A')
+describe ('Dijkstra\'s algorithm should return a binary heap of doubly linked lists', () => {
+  let temp = DijkstrasAlorithm(exampleGraph,'A','A');
   test ('it should be a doubly linked list', () => {
-    expect(temp).toBeInstanceOf(DoublyLinkedList);
+    expect(temp[0]).toBeInstanceOf(DoublyLinkedList);
   });
 
   test ('it should have a weight of 0', () => {
-    expect(temp.weight).toBe(0);
+    expect(temp[0].weight).toBe(0);
   });
 
   test ('it should have a length of 1', () => {
-    expect(temp.length).toBe(1);
+    expect(temp[0].length).toBe(1);
   });
 });
 
 describe ('Dijkstra\'s algorithm should find the shortest path between two nodes', () => {
   let temp = DijkstrasAlorithm(exampleGraph,'A','A')
   test ('It should be [\'A\']', () => {
-    expect(temp.print()).toStrictEqual(['A']);
+    expect(temp[0].print()).toStrictEqual(['A']);
   });
 
   let temp2 = DijkstrasAlorithm(exampleGraph,'A','B')
   test ('It should be [\'A\',\'B\']', () => {
-    expect(temp2.print()).toStrictEqual(['A','B']);
+    expect(temp2[0].print()).toStrictEqual(['A','B']);
   });
 
   test ('It should have a weight of 1', () => {
-    expect(temp2.weight).toBe(1);
+    expect(temp2[0].weight).toBe(1);
   });
 
   test ('It should have a length of 2', () => {
-    expect(temp2.length).toBe(2);
+    expect(temp2[0].length).toBe(2);
   });
 
   let temp3 = DijkstrasAlorithm(exampleGraph,'A','D')
   test ('It should be [\'A\',\'C\',\'D\']', () => {
-    expect(temp3.print()).toStrictEqual(['A','C','D']);
+    expect(temp3[0].print()).toStrictEqual(['A','C','D']);
   });
 
   test ('It have a weight of 3', () => {
-    expect(temp3.weight).toBe(3);
+    expect(temp3[0].weight).toBe(3);
   });
 
   test ('It have a length of 3', () => {
-    expect(temp3.length).toBe(3);
+    expect(temp3[0].length).toBe(3);
   });
 
   let temp4 = DijkstrasAlorithm(exampleGraph,'A','G')
   test ('It should be [\'A\',\'C\',\'D\',\'G\']', () => {
-    expect(temp4.print()).toStrictEqual(['A','C','D','G']);
+    expect(temp4[0].print()).toStrictEqual(['A','C','D','G']);
   });
 
   test ('It have a weight of 13', () => {
-    expect(temp4.weight).toBe(13);
+    expect(temp4[0].weight).toBe(13);
   });
 
   test ('It have a length of 4', () => {
-    expect(temp4.length).toBe(4);
+    expect(temp4[0].length).toBe(4);
   });
 
 });
