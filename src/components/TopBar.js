ac02f29b6333026eb14c624193089f4029d091dd
Ryan Scarbel <ryanscarbel@gmail.com>
2021-12-23 14:18:28 -0500

    made clear selections action more clear

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,3 +1,11 @@
+const TopBar = ({
+  reloadAction,
+  startNode,
+  endNode,
+  path,
+  clearSelections,
+  }) => {
+
 const componentStyle = {
   width: '100%',
   display: 'flex',
@@ -34,8 +42,10 @@ const newGraphButton = {
 
 const selectButton = {
   ...buttonStyles,
-  backgroundColor: '#28a745',
-  borderColor: '#28a745',
+  backgroundColor: startNode ? '#28a745' : '#C9C9C9',
+  borderColor: startNode ? '#28a745' : '#C9C9C9',
+  color: startNode ? '#FFFFFF' : '#E4E4E4',
+  pointerEvents: startNode ? '' : 'none',
 }
 
 const textStyle = {
@@ -43,34 +53,27 @@ const textStyle = {
   textAlign: 'left',
 }
 
-const TopBar = ({
-  reloadAction,
-  startNode,
-  endNode,
-  path,
-  clearSelections,
-  }) => {
 
   return <div style={componentStyle}>
-    <div style={{margin: '0 1.3em'}}>
+    <div style={{margin: '0 1.3em', flexGrow: 1}}>
     <h1 style={h1Style}>Dijkstra's<br />Algorithm</h1>
       <p>Just click nodes to get started!</p>
     </div>
-    <div style={{display:'flex',flexWrap:'wrap',flexDirection: 'column',alignContent: 'space-around', justifyContent: 'space-around'}}>
+    <div style={{display:'flex',flexWrap:'wrap',flexDirection: 'column',alignContent: 'space-around', justifyContent: 'space-around', flexGrow: 1}}>
       <button onClick={reloadAction} style={newGraphButton}>New Graph</button>
       <button onClick={true ? clearSelections : () => ''} style={selectButton}>Clear Selections</button>
     </div>
 
-    <div style={textStyle}>
+    <div style={{...textStyle, flexGrow: 1}}>
       <p><strong>Start Node</strong><br />{startNode}</p>
       <p><strong>End Node</strong><br />{endNode}</p>
     </div>
 
-    <p style={textStyle}><strong>Path:</strong> {path ? path.print().join(' -> ') : 'No Path Exists'}</p>
-    <div>
-    <p style={{marginRight: '5px',textAlign:'left'}}><strong>Total weight</strong><br />{path?.length ? path.weight : 'n/a'}</p>
-    <br />
-    <p style={{marginRight: '5px',textAlign:'left'}}><strong>Number of paths</strong><br />{path?.length ? path.length - 1 : 'n/a'}</p>
+    <p style={{...textStyle, flexGrow: 5, height: '100%', textWrap: 'wrap'}}><strong>Path:</strong> {path ? path.print().join(' -> ') : 'No Path Exists'}</p>
+    <div style={{flexGrow: 1}}>
+      <p style={{marginRight: '5px',textAlign:'left'}}><strong>Total weight</strong><br />{path?.length ? path.weight : 'n/a'}</p>
+      <br />
+      <p style={{marginRight: '5px',textAlign:'left'}}><strong>Number of paths</strong><br />{path?.length ? path.length - 1 : 'n/a'}</p>
     </div>
 
   </div>
