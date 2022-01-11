d413d420e6cc8401f51cdecfa6d538fa4ca3b74c
Ryan Scarbel <ryanscarbel@gmail.com>
2022-01-11 10:41:52 -0500

    Added optmization if max num connections is known

--- src/scripts/DijkstrasAlgorithm.ts
+++ src/scripts/DijkstrasAlgorithm.ts
@@ -9,11 +9,22 @@ import MinBinaryHeap from '../dataStructures/MinBinaryHeap';
  * @param {String} endNode
  * @returns {DoublyLinkedList} Path from start node to end node
  */
-const DijkstrasAlorithm = (graph: any, startNode: string, endNode: string) => {
+const DijkstrasAlorithm = (
+  graph: any,
+  startNode: string,
+  endNode: string,
+  maxNumberOfConnections: number = 5
+) => {
   const noPath = new DoublyLinkedList();
   noPath.addNode('', Infinity);
   noPath.length = Infinity;
   noPath.print = () => null;
+
+  //weight we will append to each calculation so that paths of equal weight but fewer nodes will be preferred
+  const extraWeight = 1 / 549755813888;
+  //if we know the highest number of direct connections any particular node has,
+  //we will only allocate a certain amount of memory to store in the binary heap.
+  const limitToBinaryHeap = 2 ** maxNumberOfConnections;
 
   const startingLinkedList = new DoublyLinkedList();
   startingLinkedList.addNode(startNode, 0);
@@ -43,19 +54,22 @@ const DijkstrasAlorithm = (graph: any, startNode: string, endNode: string) => {
         //each path gets a new linked list
         let newPath = cloneLinkedList(sortedPaths.values[0]);
         //I add a millionth to each weight so that when more than one possible route is present, it will prefer the one with fewer connections.
-        newPath.addNode(i, nodeConnections[i] + 0.000001);
+        newPath.addNode(i, nodeConnections[i] + extraWeight);
         sortedPaths.insert(newPath);
       }
     }
-    //remove slower paths that included found nodes
-    for (let i = 64; i < sortedPaths.values.length; i++) {
-      if (foundNodes.indexOf(sortedPaths.values[i].tail.name) !== -1) {
-        sortedPaths.values.splice(i, 1);
-      }
-    }
+    // //remove slower paths that included found nodes
+    // for (let i = 64; i < sortedPaths.values.length; i++) {
+    //   if (foundNodes.indexOf(sortedPaths.values[i].tail.name) !== -1) {
+    //     sortedPaths.values.splice(i, 1);
+    //   }
+    // }
     //the shortest path in the list is going to be the one we just looked at all the outgoing connections for, so let's get rid of the path to keep progressing though the graph.
     foundNodes.push(sortedPaths.extract()?.tail.name);
     foundNodes.push(sortedPaths.values[0]?.tail.name);
+
+    //remove slower paths
+    sortedPaths.values.splice(limitToBinaryHeap);
   }
 
   //If no path was found, then the first path was extracted, meaning there's no length to the linked list, so check for a length first, and if there isn't one, there isn't a path.
